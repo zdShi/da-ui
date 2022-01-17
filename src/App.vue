ef473120051455a050f4605bb8f781874a6b9acb
zhida S <chinaboy_zhida_s@sina.cn>
2022-01-18 00:02:57 +0800

    将弹窗组件更改为注册全局组件,并将创建方法挂在到Vue的原型对象上

--- src/App.vue
+++ src/App.vue
@@ -4,32 +4,46 @@
     <d-button type="success" @click="isShow = true">显示弹窗</d-button>
     <d-button type="danger" @click="isShow = false">关闭弹窗</d-button>
     <d-button type="primary" disabled>禁用</d-button>
-    <d-button type="text">文本按钮</d-button>
     <d-dialog :show.sync="isShow" msg="这是一段测试文字!!" />
+    <d-button type="text" @click="showDialog">函数式调用弹窗</d-button>
+    <d-button type="text" @click="closeDialog">关闭函数式弹窗</d-button>
   </div>
 </template>
 
 <script>
-import DButton from "./components/DButton/index";
-import DDialog from "@/components/DDialog";
+import DButton from './components/DButton/index'
+// import DDialog from '@/components/DDialog/index.vue'
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    DDialog,
-    DButton,
+    // DDialog,
+    DButton
   },
   data() {
     return {
       isShow: false,
-    };
+      //用来接收弹窗对象
+      dd: null
+    }
   },
   methods: {
     login() {
-      alert("点击登录按钮!");
+      alert('点击登录按钮!')
     },
-  },
-};
+    showDialog() {
+      this.dd = this.$dDialog({
+        msg: '这是一个函数式弹窗消息',
+        show: true,
+        //测试持续时间为0时, 是否可以手动控制
+        duration: 0
+      })
+    },
+    closeDialog() {
+      this.dd.show = false
+    }
+  }
+}
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
-
+// import './components/DDialog/index.js'
+import Dd from './plugins/message'
+Vue.use(Dd)
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
